da2aae89fe060523be3d41592305c2f64e7b2823
Hedvig <carlevag@gmail.com>
2024-10-21 13:38:49 +0200

    Last touches

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-function toggleDropdown(index) {
-
+function toggleDropdown(button, index) {
     const dropdown = document.getElementById(`dropdown${index}`);
     const hr = document.getElementById(`hr${index}`);
+    const imgElement = button.querySelector('img'); 
 
     dropdown.classList.toggle("show");
-    
+
     if (dropdown.classList.contains("show")) {
-        hr.style.display = "none";  
+        hr.style.display = "none"; 
     } else {
         hr.style.display = "block"; 
+    }
+
+    if (imgElement.src.includes('icon-plus.svg')) {
+        imgElement.src = 'assets/images/icon-minus.svg'; 
+    } else {
+        imgElement.src = 'assets/images/icon-plus.svg'; 
     }
 }
