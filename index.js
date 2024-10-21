a9cba393ce173647aa35d002463a1a76bfdf70b0
Hedvig <carlevag@gmail.com>
2024-10-21 10:04:25 +0200

    fully working hr display

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 function toggleDropdown(index) {
-    // Toggle the dropdown corresponding to the clicked button
-    document.getElementById(`dropdown${index}`).classList.toggle("show");
-  }
-  
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.closest('.image-button')) {
-      let dropdowns = document.getElementsByClassName("dropdown-content");
-      for (let i = 0; i < dropdowns.length; i++) {
-        let openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
+
+    const dropdown = document.getElementById(`dropdown${index}`);
+    const hr = document.getElementById(`hr${index}`);
+
+    dropdown.classList.toggle("show");
+    
+    if (dropdown.classList.contains("show")) {
+        hr.style.display = "none";  
+    } else {
+        hr.style.display = "block"; 
     }
-  };
-  
+}
